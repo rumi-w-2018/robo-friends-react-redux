37a0b60c977a73c01c7d888c095e532d4c9899a5
Harumi Warner <harumiwarner@gmail.com>
2018-08-17 12:23:50 -0600

    Added new files.

--- src/actions/searchKey.js
+++ src/actions/searchKey.js
@@ -1,8 +1,8 @@
 import { SET_SEARCH_KEY } from './constants';
 
-export const setSearchKey = (key) => {
-    return {
-        type: SET_SEARCH_KEY,
-        payload: key
-    };
+export const setSearchKey = key => {
+  return {
+    type: SET_SEARCH_KEY,
+    payload: key
+  };
 };
